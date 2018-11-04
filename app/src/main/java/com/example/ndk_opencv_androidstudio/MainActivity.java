16e04c5723a8c8e427e34ba3195d6b4ae95cd074
guanfuchen <guanfuchen@zju.edu.cn>
2018-11-04 23:13:55 +0800

    增加lbp人脸检测模块

--- app/src/main/java/com/example/ndk_opencv_androidstudio/MainActivity.java
+++ app/src/main/java/com/example/ndk_opencv_androidstudio/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ndk_opencv_androidstudio;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -10,8 +15,18 @@ import android.view.SurfaceView;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -30,12 +45,36 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
         setContentView(R.layout.activity_main);
 
         NativeClass.getStringFromNative();
+        initClassifier();
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(1);
 
 //        Mat mat = new Mat();
+    }
+
+    private CascadeClassifier classifier;
+    // 初始化人脸级联分类器，必须先初始化
+    private void initClassifier() {
+        try {
+//            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface_improved);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File cascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+            FileOutputStream os = new FileOutputStream(cascadeFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+            classifier = new CascadeClassifier(cascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -72,13 +111,7 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
     @Override
     protected void onResume() {
         super.onResume();
-//                if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this,
-//                        mLoaderCallback)) {
-//                    Log.i(TAG, "没有加载OpenCvManager");
-//                }
-
         mOpenCvCameraView.enableView();
-        //        System.loadLibrary("opencv_java");
     }
 
     @Override
@@ -113,13 +146,39 @@ public class MainActivity extends ActionBarActivity implements CameraBridgeViewB
 
     }
 
+    private int mAbsoluteFaceSize = 0;
+    private float mRelativeFaceSize = 0.2f;
+
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Log.e(TAG, "onCameraFrame----in----");
         Mat rgbaMat = inputFrame.rgba();
-        Mat greyMat = new Mat();
-        Imgproc.cvtColor(rgbaMat, greyMat, 7);
+        Mat grayMat = inputFrame.gray();
+
+        if (mAbsoluteFaceSize == 0) {
+            int height = grayMat.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+
+//        Core.flip(rgbaMat, rgbaMat, -1);
+//        Core.flip(grayMat, grayMat, -1);
+
+//        Mat greyMat = new Mat();
+//        Imgproc.cvtColor(rgbaMat, greyMat, 7);
+//        return greyMat;
+        MatOfRect faces = new MatOfRect();
+        if (classifier != null) {
+            classifier.detectMultiScale(grayMat, faces, 1.1, 2, 2, new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        }
+        Rect[] facesArray = faces.toArray();
+        Scalar faceRectColor = new Scalar(0, 255, 0, 255);
+        for (Rect faceRect : facesArray) {
+            Core.rectangle(rgbaMat, faceRect.tl(), faceRect.br(), faceRectColor, 3);
+        }
+
         Log.e(TAG, "onCameraFrame----out----");
-        return greyMat;
+        return rgbaMat;
     }
 }
